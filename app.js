61730e278ef47d18c53e78084938e355b26abb4b
André Lima <andreluis017@gmail.com>
2022-06-23 18:28:48 -0300

    final project

--- app.js
+++ app.js
@@ -1,2 +1,18 @@
 //using selectors inside the element
 // traversing the dom
+
+const questions = document.querySelectorAll('.question');
+
+questions.forEach(function(question){
+    const btn = question.querySelector('.question-btn')
+    btn.addEventListener('click', function(){
+
+        questions.forEach(function(question2){
+            if(question2 !== question){
+                question2.classList.remove('show-text')
+            }
+        })
+
+        question.classList.toggle('show-text')
+    })
+})
